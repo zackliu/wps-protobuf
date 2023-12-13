552714988bdee61f85ccce5ad7712eb7ee73b239
Chenyang Liu <chenyl@microsoft.com>
2023-12-13 10:19:53 +0800

    init

---
+++ WebPubSubSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using Azure.Messaging.WebPubSub.Clients;
+
+var client = new WebPubSubClient(new Uri("<client-access-uri>"));
+
+client.GroupMessageReceived += eventArgs =>
+{
+    Console.WriteLine($"Receive group message from {eventArgs.Message.Group}: {eventArgs.Message.Data}");
+    return Task.CompletedTask;
+};
+
+await client.StartAsync();
+
+await client.SendToGroupAsync("testGroup", BinaryData.FromString("hello world"), WebPubSubDataType.Text);
